b659247449804b2f92628adb2e33d114d3f434e8
jj <jjaywallace@gmail.com>
2022-06-03 15:21:10 -0700

    button fix

--- src/components/menu/Button.js
+++ src/components/menu/Button.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import gsap from "gsap";
 
-class Car extends React.Component {
+function Button() {
 
-	render() {
-		buttonState = 1;
+	var buttonState = 1;
 
-		handleClick(){
-			console.log('click')
-			if(buttonState == 1){
-				gsap.to(".button", {scale: 0.5, duration: 0.5, ease: "strong.inOut"});
-				buttonState = 0
-			}else{
-				gsap.to(".button", {scale: 1, duration: 0.5, ease: "strong.inOut"});
-				buttonState = 1;
-			}
-			//gsap.to(".button", {scale: 0, x: -300, backgroundColor: "#4CAF50", duration: 0.5, ease: "strong.inOut"});
-
+	function handleClick(){
+		console.log('click')
+		if(buttonState == 1){
+			gsap.to(".button", {scale: 0.5, duration: 0.5, ease: "strong.inOut"});
+			buttonState = 0
+		}else{
+			gsap.to(".button", {scale: 0, duration: 0.5, ease: "strong.inOut"});
+			buttonState = 1
 		}
+		//gsap.to(".button", {scale: 0, x: -300, backgroundColor: "#4CAF50", duration: 0.5, ease: "strong.inOut"});
 
-		gsap.to(".button", {scale: 1, backgroundColor: "#4CAF50", duration: 1, ease: "strong.inOut"});
-
-		return (
-			<div className="button" onClick={handleClick}>
-				HELLO
-			</div>
-		);
 	}
+
+	gsap.to(".button", {scale: 1, backgroundColor: "#4CAF50", duration: 1, ease: "strong.inOut"});
+
+  return (
+    <div className="button" onClick={handleClick}>
+      HELLO
+    </div>
+  );
 }
 
 export default Button;
