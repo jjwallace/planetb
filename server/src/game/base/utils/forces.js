e662e5ea52819733e70dcbed0db89389a4bd7b8b
jj <jjaywallace@gmail.com>
2022-07-01 19:08:17 -0700

    moved utils to shared folder, got surface items on planet

--- server/src/game/base/utils/forces.js
+++ server/src/game/base/utils/forces.js
@@ -1,18 +1,18 @@
 export function forces(entity) {
-	//if(entity.type == 'unit'){
-		try {
-			if ('location' in entity && 'velocity' in entity){
-				entity.location.x += entity.velocity.x;
-				entity.location.y += entity.velocity.y;
-				entity.location.r += entity.velocity.r;
-		}
-		}catch (error){
-			console.warn('Entity does not of acc or location', error)
-		}
-	
-	//}
+  //if(entity.type == 'unit'){
+  try {
+    if ('location' in entity && 'velocity' in entity) {
+      entity.location.x += entity.velocity.x;
+      entity.location.y += entity.velocity.y;
+      entity.location.r += entity.velocity.r;
+    }
+  } catch (error) {
+    console.warn('Entity does not of acc or location', error)
+  }
 
-	return {
-		entity
-	};
-} 
+  //}
+
+  return {
+    entity
+  };
+}
--- server/src/game/base/utils/surface.js
+++ server/src/game/base/utils/surface.js
@@ -1,14 +1,10 @@
 export function surface(entities, entity) {
 	//if(entity.type == 'unit'){
 		try {
-			entity.location.s += 0.01;
-			let surfaceLocation = entity.location.s;
-
 			var parentEntity = entities.find(e => e.uuid === entity.parent);
+            entity.location.x = parentEntity.features.size * Math.sin(entity.location.s); // X Component of Force
+            entity.location.y = parentEntity.features.size * Math.cos(entity.location.s); // Y Component of Force
 
-            //ntity.rotation = surfaceLocation;
-            entity.x = 300;//parentEntity.features.size * Math.sin(surfaceLocation) * 100; // X Component of Force
-            entity.y = 300;// parentEntity.features.size * Math.cos(surfaceLocation) * 100; // Y Component of Force
 		}catch (error){
 			console.warn('Entity does not of acc or location', error)
 		}
